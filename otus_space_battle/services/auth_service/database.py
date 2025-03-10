88b44ff38d83e6e31165ef5dc2cdafbf0a3359a3
Shidimmah <Shibboleth=2>
2025-03-11 01:26:36 +0300

    Создание таблицы юзеров в БД

--- otus_space_battle/services/auth_service/database.py
+++ otus_space_battle/services/auth_service/database.py
@@ -11,3 +11,8 @@ Base = declarative_base()
 async def get_db():
     async with SessionLocal() as session:
         yield session
+
+# ✅ Добавляем создание таблиц при старте сервиса
+async def init_db():
+    async with engine.begin() as conn:
+        await conn.run_sync(Base.metadata.create_all)
