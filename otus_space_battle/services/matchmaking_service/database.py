825f20873883322e8043ed59083dff9ab8b658cb
Shidimmah <Shibboleth=2>
2025-03-15 14:23:27 +0300

    Исправлен сервис матчей

--- otus_space_battle/services/matchmaking_service/database.py
+++ otus_space_battle/services/matchmaking_service/database.py
@@ -1,8 +1,7 @@
 import sys
 import os
 
-# Добавляем корневую директорию проекта в sys.path
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..", "..")))
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..", "..", "services")))
 
 from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
 from sqlalchemy.orm import sessionmaker
@@ -14,8 +13,8 @@ engine = create_async_engine(DATABASE_URL, echo=True)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine, class_=AsyncSession)
 Base = declarative_base()
 
-from services.auth_service.models import User
-from services.matchmaking_service.models import Match
+from auth_service.models import User
+from matchmaking_service.models import Match
 
 async def init_db():
     async with engine.begin() as conn:
