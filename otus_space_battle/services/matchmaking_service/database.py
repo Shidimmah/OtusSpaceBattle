be55c713acdfd2ffe2f5967d0de6b9dd4cecc200
Shidimmah <Shibboleth=2>
2025-03-15 14:18:11 +0300

    Исправлена старая ошибка автосоздания БД матчей

--- otus_space_battle/services/matchmaking_service/database.py
+++ otus_space_battle/services/matchmaking_service/database.py
@@ -1,6 +1,7 @@
 import sys
 import os
 
+# Добавляем корневую директорию проекта в sys.path
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..", "..")))
 
 from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
@@ -8,12 +9,13 @@ from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
 DATABASE_URL = "postgresql+asyncpg://user:password@db/main_db"
+
 engine = create_async_engine(DATABASE_URL, echo=True)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine, class_=AsyncSession)
 Base = declarative_base()
 
-from otus_space_battle.services.auth_service.models import User
-from otus_space_battle.services.matchmaking_service.models import Match
+from services.auth_service.models import User
+from services.matchmaking_service.models import Match
 
 async def init_db():
     async with engine.begin() as conn:
