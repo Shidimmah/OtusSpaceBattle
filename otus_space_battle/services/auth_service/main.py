2f3974220827f70a8c5291c8a2e6b037835f82eb
Shidimmah <Shibboleth=2>
2025-03-15 15:25:42 +0300

    Рефакторинг БД

--- otus_space_battle/services/auth_service/main.py
+++ otus_space_battle/services/auth_service/main.py
@@ -1,5 +1,17 @@
 from fastapi import FastAPI
 from auth import router as auth_router
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
+from models import Base  # Подключаем модели из единого источника
+
+DATABASE_URL = "postgresql+asyncpg://user:password@database_service/main_db"
+
+engine = create_async_engine(DATABASE_URL, echo=True)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine, class_=AsyncSession)
+
+async def get_db():
+    async with SessionLocal() as session:
+        yield session
 
 app = FastAPI(title="Auth Service")
 
