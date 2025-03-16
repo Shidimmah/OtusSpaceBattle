01ef5401862cb06a9f2b4fa54dc2e76cc073eb49
Shidimmah <Shibboleth=2>
2025-03-16 16:14:58 +0300

    Рефакторинг моделей 5

--- otus_space_battle/services/matchmaking_service/models.py
+++ otus_space_battle/services/matchmaking_service/models.py
@@ -1,1 +1,7 @@
-from database_service.models.models import Match  # Теперь берём из database_service
+import sys
+import os
+
+# Добавляем путь к database_service в sys.path
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '../database_service')))
+
+from models import Match  # Теперь берём из database_service
