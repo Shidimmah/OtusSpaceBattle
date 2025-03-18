394c503db668c20cd33db054251bd3aea0a09758
Shidimmah <Shibboleth=2>
2025-03-18 23:34:14 +0300

    Исправлены пути в Dockerfile и URL'ы сервисов в api_gateway

--- otus_space_battle/services/api_gateway/main.py
+++ otus_space_battle/services/api_gateway/main.py
@@ -8,8 +8,8 @@ from prometheus_client import start_http_server
 
 app = FastAPI()
 
-AUTH_SERVICE_URL = "http://auth_service:8001"
-MATCHMAKING_SERVICE_URL = "http://matchmaking_service:8002"
+AUTH_SERVICE_URL = "http://auth_service:8000"
+MATCHMAKING_SERVICE_URL = "http://matchmaking:8000"
 
 SECRET_KEY = "your_secret_key"  # ❗ Должен совпадать с ключом из `auth_service`
 
@@ -51,7 +51,7 @@ async def proxy_auth(path: str, request: Request):
     return await proxy_request(target_url, request)
 
 @app.api_route("/matchmaking/{path:path}", methods=["GET", "POST", "PUT", "DELETE"])
-async def proxy_auth(path: str, request: Request):
+async def proxy_matchmaking(path: str, request: Request):
     target_url = f"{MATCHMAKING_SERVICE_URL}/matchmaking/{path}"
     return await proxy_request(target_url, request)
 
